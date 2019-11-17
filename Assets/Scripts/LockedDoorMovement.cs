fa612164d4649a45837651a070993a5d06ccf45d
uo264074 <56980891+uo264074@users.noreply.github.com>
2019-11-17 18:18:53 +0100

    Añadidos scripts
    Añadida la carpeta "Scripts" con los que controlan el movimiento y las distintas puertas.

---
+++ Assets/Scripts/LockedDoorMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoorMovement : MonoBehaviour
+{
+
+    // Es necesario asignar este script a un Empty Object, situado en el eje de rotación
+    // de la puerta, y hacer a esta su hija. A este objeto se le debe añadir un componente
+    // Box Collider, con el check "Is Trigger" marcado.
+    // Si, habiendo recogido la llave, el jugador está dentro del collider y pulsa el botón
+    // indicado, la puerta se abre o cierra.
+
+    public float doorOpenAngle = 90.0f; // Ángulo de apertura de la puerta
+    public float openSpeed = 2.0f; // Velocidad de apertura
+
+    bool open = false;
+    bool enter = false;
+
+    float defaultRotationAngle;
+    float currentRotationAngle;
+    float openTime = 0;
+
+    public static bool hasKey = false;
+
+    void Start()
+    {
+        defaultRotationAngle = transform.localEulerAngles.y;
+        currentRotationAngle = transform.localEulerAngles.y;
+    }
+
+    // Main function
+    void Update()
+    {
+        if(openTime < 1)
+        {
+            openTime += Time.deltaTime * openSpeed;
+        }
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Mathf.LerpAngle(currentRotationAngle, defaultRotationAngle + (open ? doorOpenAngle : 0), openTime), transform.localEulerAngles.z);
+
+        // Para abrir con el mando, se necesita configurar botón "Fire3" en el editor
+        // Joystick button 2 para botón CUADRADO en el móvil
+        if (Input.GetButtonDown("Fire3") && enter)
+        {
+            if (hasKey) {
+                open = !open;
+                currentRotationAngle = transform.localEulerAngles.y;
+                openTime = 0;
+                // Reporudcir aquí sonido de puerta abriéndose
+            } else {
+                //Reproducir sonido de puerta bloqueada
+            }
+        }
+
+        // Para abrir con el teclado
+        if (Input.GetKeyDown("space") && enter)
+        {
+            if (hasKey) {
+                open = !open;
+                currentRotationAngle = transform.localEulerAngles.y;
+                openTime = 0;
+                // Reporudcir aquí sonido de puerta abriéndose
+            } else {
+                //Reproducir sonido de puerta bloqueada
+            }
+        }
+    }
+
+    // Enseña un mensaje en pantalla (en el móvil con VR no funciona)
+    void OnGUI()
+    {
+        if (enter)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 75, Screen.height - 100, 150, 50), "Press 'SPACE' to interact with the door");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enter = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enter = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Importar al proyecto los assets para el Google Cardboard y añadir a la escena
+    // un GvrEditorEmulator.
+    // La cámara debe ser hija de un Empty Object al que se le asignará este script.
+    // Desde el editor de Unity, arrastrar la cámara de la escena al atributo 
+    // "Main Camera" del script.
+
+    public float horizontalMove;
+    public float verticalMove;
+    private Vector3 movementInput;
+    private Vector3 movePlayer;
+
+    public CharacterController player;
+
+    public float speed;
+
+    public float gravity = 9.8f;
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();	
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Para mando, INPUT en configuración del editor > X Axis
+        horizontalMove = Input.GetAxis("Horizontal");
+        // Para mando, INPUT en configuración del editor > Y Axis
+        verticalMove = Input.GetAxis("Vertical");
+
+        movementInput = new Vector3(horizontalMove, 0, verticalMove) * speed;
+
+        lookingAt();
+
+        movePlayer = movementInput.x * camRight + movementInput.z * camForward;
+
+        //player.transform.LookAt(player.transform.position + movePlayer);
+
+        SetGravity();
+        player.Move(movePlayer);
+    }
+
+    void SetGravity() {
+        movePlayer.y = -gravity * Time.deltaTime;
+    }
+
+    void lookingAt() {
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+    }
+}
