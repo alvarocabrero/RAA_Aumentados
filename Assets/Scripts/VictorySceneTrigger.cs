77cc075d651d1ca5e56c83f48a25589193a92dda
uo264074 <56980891+uo264074@users.noreply.github.com>
2019-11-21 18:28:52 +0100

    Añadida la escena de victoria
    Añadida la escena de victoria, sus controles, y la música que se reproduce en bucle

---
+++ Assets/Scripts/VictorySceneTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictorySceneTrigger : MonoBehaviour
+{
+    void Start() {}
+
+    void Update() {}
+
+    // Cuando el jugador entre en la zona tras la puerta final, se carga la escena de victoria
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            SceneManager.LoadScene("VictoryScene");
+            LockedDoorMovement.hasKey = false;
+        }
+    }
+}
