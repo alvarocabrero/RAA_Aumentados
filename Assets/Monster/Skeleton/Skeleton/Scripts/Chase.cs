f46cb417099169fc34edb983888c9bb39d02ca98
nicomencia <nicomencia4@gmail.com>
2019-11-24 19:47:16 +0100

    Monstruo animaciones y scripts

---
+++ Assets/Monster/Skeleton/Skeleton/Scripts/Chase.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    public Transform player;
+    static Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - this.transform.position;
+        float angle = Vector3.Angle(direction,this.transform.forward);
+
+        //Si el jugador está a menos de 50 de distancia y en el ángulo de vision del monstruo este se mueve hacia él
+        if(Vector3.Distance(player.position, this.transform.position) < 50 && angle < 60)
+        {
+            //No se quiere modificar el eje y
+            direction.y = 0;
+
+            //El monstruo mira al jugador, slerp para una rotación más natural
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                        Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("isIdle",false);
+            //Si el jugador está a menos de 10 unidades del monstruo este se para y le ataca
+             if(direction.magnitude > 10)
+             {
+                this.transform.Translate(0,0,0.07f);
+                anim.SetBool("isWalking",true);
+                anim.SetBool("isAttacking",false);
+             }
+             else
+             {
+                anim.SetBool("isAttacking",true);
+                anim.SetBool("isWalking",false);
+             }
+        }
+        else
+        {
+            anim.SetBool("isIdle",true);
+            anim.SetBool("isAttacking",false);
+            anim.SetBool("isWalking",false);
+        }
+    }
+}
---
+++ Assets/Monster/Skeleton/Skeleton/Scripts/Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    NavMeshPath path;
+    public float timeForNewPath;
+    bool inCoRoutine;
+    static Animator anim;
+    Vector3 target;
+    bool validPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        path = new NavMeshPath();
+        anim = GetComponent<Animator>();
+    }
+
+    Vector3 getNewRandomPosition()
+    {
+        float x = Random.Range(-100,100);
+        float z = Random.Range(-100,100);
+
+        Vector3 pos = new Vector3(x,0,z);
+        return pos;
+    }
+
+    IEnumerator Move()
+    {
+        inCoRoutine = true;
+        yield return new WaitForSeconds(timeForNewPath);
+        GetNewPath();
+        validPath = navMeshAgent.CalculatePath(target,path);
+
+        while(!validPath)
+        {
+            yield return new WaitForSeconds(0.01f);
+            GetNewPath();
+            validPath = navMeshAgent.CalculatePath(target,path);
+        }
+        inCoRoutine = false;
+    }
+
+    void GetNewPath()
+    {
+        anim.SetBool("isIdle",false);
+        anim.SetBool("isAttacking",false);
+        anim.SetBool("isWalking",true);
+        target = getNewRandomPosition();
+        navMeshAgent.SetDestination(target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!inCoRoutine)
+        {
+        StartCoroutine(Move());
+        }
+    }
+}
